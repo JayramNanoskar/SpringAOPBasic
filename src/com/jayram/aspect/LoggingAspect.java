207eaee5ae4ac5c706dade4435a7bf18710c65c2
JayramNanoskar <jayram.n978@gmail.com>
2021-06-19 15:46:50 +0530

    using Wildcard expressions

--- src/com/jayram/aspect/LoggingAspect.java
+++ src/com/jayram/aspect/LoggingAspect.java
@@ -6,7 +6,7 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String getName())")
+	@Before("execution(public * get*(..))") //Using wild card expression to execute all getters irrespective of any return type & arguments.
 	public void loggingAdvise(){
 		System.out.println("Advice run. Get method called.");
 	}
