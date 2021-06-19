370b121d47ea583462716128bfd9cea0315b159e
JayramNanoskar <jayram.n978@gmail.com>
2021-06-19 16:20:08 +0530

    using Pointcuts

--- src/com/jayram/aspect/LoggingAspect.java
+++ src/com/jayram/aspect/LoggingAspect.java
@@ -2,12 +2,21 @@ package com.jayram.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public * get*(..))") //Using wild card expression to execute all getters irrespective of any return type & arguments.
+	@Before("allGetters()")
 	public void loggingAdvise(){
 		System.out.println("Advice run. Get method called.");
 	}
+
+	@Before("allGetters()")
+	public void secondAdvice(){
+		System.out.println("Second Advice executed.");
+	}
+
+	@Pointcut("execution(public * get*(..))")
+	public void allGetters(){}
 }
